4823df4ce6c544160b02702a5f9648e4d17707c7
Nathaniel Goh <gohnathaniel2000@gmail.com>
2021-05-04 17:01:06 -0700

    testing some yfinance stuff

--- stock.py
+++ stock.py
@@ -1,3 +1,8 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import ta
+
 class Stock():
     def __init__(self, symbol):
         """
@@ -9,3 +14,12 @@ class Stock():
             Get the stock symbol you want to predict
         """
         self.symbol = symbol
+
+    def get_historical_data(self, period = '2y', interval = '1d'):
+        """
+        Get the historical stock data using the yfinance API
+        """
+        stock = yf.Ticker(self.symbol)
+        return stock.history(period = period, interval = interval)
+        
+    
