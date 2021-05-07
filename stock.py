d49ddbbfa44b2b7f392082040a1b01e196ca8856
Nathaniel Goh <gohnathaniel2000@gmail.com>
2021-05-06 17:32:42 -0700

    update preprocessing

--- stock.py
+++ stock.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np
 import ta
 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from sklearn import preprocessing
+
 class Stock():
     def __init__(self, symbol):
         """
@@ -18,18 +23,36 @@ class Stock():
     def get_data(self, period = '2y', interval = '1d'):
         """
         Get the historical stock data using the yfinance API
+        Then pre-process data to be used for training and testing
+
+        Parameters
+        ----------
+        period : str
+            How far back we want to get historical data using
+        interval : str
+            Interval of historical data
         """
+        
+        # Get data
         stock = yf.Ticker(self.symbol)
         df = stock.history(period = period, interval = interval)
         df.dropna(inplace = True)
+
+        # Get the features we want to use
         df = ta.add_all_ta_features(df, 'Open', 'High', 'Low', 'Close', 'Volume', fillna = True)
         df.drop(['Open', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits'], axis = 1, inplace = True)
+
+        # Scaling the features
+        # scaled_close_prices = preprocessing.RobustScaler()
+        # scaled_close_prices.fit(df[['Close']])
+
+        scaled_df = preprocessing.RobustScaler()
+        df = pd.DataFrame(scaled_df.fit_transform(df), columns = df.columns, index = df.index)
 
         return df
     
     def get_train_test(self, df):
-        train_length = int(len(df) * 0.8)
+        forecast = 30 # How many days we want to predict into the future
 
-        train = df.iloc[:train_length, :]
-        test = df.iloc[train_length:, :]
+        y = 
     
