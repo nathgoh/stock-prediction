3eb925fe79b358427e9485918699c4730861eca3
Nathaniel Goh <gohnathaniel2000@gmail.com>
2021-05-16 22:39:04 -0700

    adding user input and some model eval

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from stock import Stock
 import numpy as np
 
+import tensorflow as tf
+from tensorflow import keras
+
 def main():
-    stock = Stock('SPY')
+
+    stock_symbol = input("Enter stock symbol: ")
+    stock = Stock(stock_symbol.upper())
     df = stock.get_data()
     X, y = stock.get_train_test(df)
 
     model = stock.make_model(df.shape[1])
-    model.summary()
 
-    trained_model = model.fit(X, y, epochs = 50, batch_size = 128, validation_split=0.2)
+    callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=1e-4, patience=8)
+    trained_model = model.fit(X, y, epochs = 50, batch_size = 128, validation_split=0.2, callbacks = [callback])
     
+    pred = stock.evaluate_model(df, trained_model)
+
 if __name__ == '__main__':
     main()
--- stock.py
+++ stock.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import ta
 
-# from sklearn import preprocessing
-# from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import RobustScaler
+
 
 import tensorflow as tf
 from tensorflow import keras
@@ -43,14 +43,14 @@ class Stock():
 
         # Get the features we want to use
         df = ta.add_all_ta_features(df, 'Open', 'High', 'Low', 'Close', 'Volume', fillna = True)
-        df.drop(['Open', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits'], axis = 1, inplace = True)
-        # df_closed = df[['Close']]
+        df.drop(['Open', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits'], axis = 1, inplace = True) 
+        # df = pd.DataFrame(RobustScaler().fit_transform(df), columns = df.columns, index = df.index)
 
         return df
     
     def get_train_test(self, df):
         df_array = df.to_numpy()
-        train_forecast = 120 # How many days of past data you want to train for forecasting
+        train_forecast = 240 # How many days of past data you want to train for forecasting
         forecast = 30 # How many days we want to predict into the future
 
         X, y = [], []
@@ -68,24 +68,32 @@ class Stock():
         y = np.array(y)
         return X, y
 
-    def make_model(self, num_features, train_forecast_size = 120):
+    def make_model(self, num_features, train_forecast = 240):
         model = Sequential()    
 
         model.add(layers.LSTM(
-            units = train_forecast_size, 
+            units = train_forecast, 
             activation= 'tanh',
             return_sequences = True,
-            input_shape = (train_forecast_size, num_features)
+            input_shape = (train_forecast, num_features)
             ))
 
         model.add(layers.LSTM(
-            units = int(train_forecast_size * 0.75), 
+            units = int(train_forecast * 0.75), 
             activation = 'tanh',
             return_sequences = True,
             ))
 
         model.add(layers.LSTM(
-            units = train_forecast_size // 2, 
+            units = int(train_forecast * 0.6), 
+            activation = 'tanh',
+            return_sequences = True,
+            ))
+
+        model.add(layers.Dropout(0.1))
+
+        model.add(layers.LSTM(
+            units = train_forecast // 2, 
             activation = 'tanh',
             return_sequences = True,
             ))
@@ -93,7 +101,7 @@ class Stock():
         model.add(layers.Dropout(0.2))
 
         model.add(layers.LSTM(
-            units = train_forecast_size // 4, 
+            units = train_forecast // 4, 
             activation = 'tanh'
             ))
 
@@ -104,3 +112,16 @@ class Stock():
 
         return model
       
+    def evaluate_model(self, df, trained_model, train_forecast = 240, forecast = 30):
+        predictions = pd.DataFrame(index = df.index, columns = [df.columns[0]])
+        for j in range(train_forecast, len(df) - train_forecast, forecast):
+            X = df[-j - train_forecast:-j]
+
+            # Prediction per train_forecast size
+            y_hat = model.predict(np.array(X).reshape(1, train_forecast, df.shape[0]))
+
+            # Update predictions
+            pred_df = pd.DataFrame(y_hat, index=pd.date_range(start=X.index[-1], periods=len(y_hat), freq="B"), columns=[X.columns[0]])
+            predictions.update(pred_df) 
+
+            
