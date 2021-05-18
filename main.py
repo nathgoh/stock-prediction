8ccf3e429ec85693e7ea3551734fcd423bc6dd75
Nathaniel Goh <gohnathaniel2000@gmail.com>
2021-05-17 22:45:52 -0700

    made model and did some figure visualization

--- main.py
+++ main.py
@@ -1,22 +1,117 @@
 from stock import Stock
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+import sklearn.preprocessing as preprocessing
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers, optimizers
+
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
 
 def main():
 
-    stock_symbol = input("Enter stock symbol: ")
-    stock = Stock(stock_symbol.upper())
+    # stock_symbol = input("Enter stock symbol: ")
+    stock = Stock('SPY')
     df = stock.get_data()
-    X, y = stock.get_train_test(df)
 
-    model = stock.make_model(df.shape[1])
+    df = df[['Close']]
 
-    callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=1e-4, patience=8)
-    trained_model = model.fit(X, y, epochs = 50, batch_size = 128, validation_split=0.2, callbacks = [callback])
-    
-    pred = stock.evaluate_model(df, trained_model)
+    train_dates = df.index[:int(df.shape[0] * 0.8)]
+    test_dates = df.index[int(df.shape[0] * 0.8):]
+
+    num_features = df.shape[1]
+    train_df = np.array(df[:int(df.shape[0] * 0.8)])
+    test_df = np.array(df[int(df.shape[0] * 0.8):])
+
+    scaler = preprocessing.RobustScaler()
+    train_df = scaler.fit_transform(train_df)
+    test_df = scaler.transform(test_df)
+
+    X_train, y_train, used_dates_train = create_dataset(train_df, train_dates)
+    X_test, y_test, used_dates_test = create_dataset(test_df, test_dates)
+
+    model = make_model(num_features)
+    callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=1e-5, patience=8)
+    history = model.fit(X_train, y_train, epochs = 50, batch_size = 64, validation_data=(X_test, y_test), callbacks = [callback])
+ 
+    predictions = model.predict(X_test)
+    predictions = scaler.inverse_transform(predictions)
+    y_test = scaler.inverse_transform(y_test.reshape(-1, 1))
+
+    plot_data(predictions, y_test, used_dates_test)
+
+def plot_data(predictions, y_test, dates):
+    fig, axes = plt.subplots(figsize=(16, 8))
+    axes.plot(y_test, color = 'red', label = 'Original Price')
+    plt.plot(predictions, color = 'cyan', label = 'Predicted Price')
+    plt.ylabel("Price (USD)")
+    plt.legend()
+    plt.show()
+
+def create_dataset(df, dates, look_back = 90):    
+    X, y, used_dates = [], [], []
+
+    for j in range(look_back, df.shape[0]):
+        X.append(df[j - look_back:j, :])
+        y.append(df[j, 0])
+        used_dates.append(dates[j])
+
+    X = np.array(X)
+    y = np.array(y)
+    used_dates = np.array(used_dates)
+
+    return X, y, used_dates
+
+def make_model(num_features, look_back = 90):
+    model = Sequential()    
+
+    model.add(layers.LSTM(
+        units = 120, 
+        activation= 'tanh',
+        return_sequences = True,
+        input_shape = (look_back, num_features)
+        ))
+
+    model.add(layers.LSTM(
+        units = 120, 
+        activation = 'tanh',
+        return_sequences = True,
+        ))
+
+    model.add(layers.Dropout(0.2))
+
+    model.add(layers.LSTM(
+        units = 120, 
+        activation = 'tanh',
+        return_sequences = True,
+        ))
+
+    model.add(layers.Dropout(0.2))
+
+    model.add(layers.LSTM(
+        units = 120, 
+        activation = 'tanh',
+        return_sequences = True,
+        ))
+
+    model.add(layers.Dropout(0.2))
+
+    model.add(layers.LSTM(
+        units = 120, 
+        activation = 'tanh'
+        ))
+
+    model.add(layers.Dense(1))
+
+    optimizer = optimizers.Adam(learning_rate = 0.001)
+    model.compile(optimizer = optimizer, loss = 'mse', metrics=['accuracy'])
+
+    return model
+      
 
 if __name__ == '__main__':
     main()
--- stock.py
+++ stock.py
@@ -2,15 +2,6 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import ta
-
-from sklearn.preprocessing import RobustScaler
-
-
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras import layers, optimizers
-
 class Stock():
     def __init__(self, symbol):
         """
@@ -23,7 +14,7 @@ class Stock():
         """
         self.symbol = symbol
 
-    def get_data(self, period = '2y', interval = '1d'):
+    def get_data(self, period = '10y', interval = '1d'):
         """
         Get the historical stock data using the yfinance API
         Then pre-process data to be used for training and testing
@@ -42,86 +33,9 @@ class Stock():
         df.dropna(inplace = True)
 
         # Get the features we want to use
-        df = ta.add_all_ta_features(df, 'Open', 'High', 'Low', 'Close', 'Volume', fillna = True)
-        df.drop(['Open', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits'], axis = 1, inplace = True) 
-        # df = pd.DataFrame(RobustScaler().fit_transform(df), columns = df.columns, index = df.index)
+        # df = ta.add_all_ta_features(df, 'Open', 'High', 'Low', 'Close', 'Volume', fillna = True)
+        # df.drop(['Open', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits'], axis = 1, inplace = True) 
 
         return df
     
-    def get_train_test(self, df):
-        df_array = df.to_numpy()
-        train_forecast = 240 # How many days of past data you want to train for forecasting
-        forecast = 30 # How many days we want to predict into the future
-
-        X, y = [], []
-        for j in range(len(df_array)):
-            train_end = j + train_forecast
-            end = train_end + forecast
-
-            if end > len(df_array):
-                break
-
-            X.append(df_array[j:train_end, :])
-            y.append(df_array[train_end:end, 0])
-
-        X = np.array(X)
-        y = np.array(y)
-        return X, y
-
-    def make_model(self, num_features, train_forecast = 240):
-        model = Sequential()    
-
-        model.add(layers.LSTM(
-            units = train_forecast, 
-            activation= 'tanh',
-            return_sequences = True,
-            input_shape = (train_forecast, num_features)
-            ))
-
-        model.add(layers.LSTM(
-            units = int(train_forecast * 0.75), 
-            activation = 'tanh',
-            return_sequences = True,
-            ))
-
-        model.add(layers.LSTM(
-            units = int(train_forecast * 0.6), 
-            activation = 'tanh',
-            return_sequences = True,
-            ))
-
-        model.add(layers.Dropout(0.1))
-
-        model.add(layers.LSTM(
-            units = train_forecast // 2, 
-            activation = 'tanh',
-            return_sequences = True,
-            ))
-
-        model.add(layers.Dropout(0.2))
-
-        model.add(layers.LSTM(
-            units = train_forecast // 4, 
-            activation = 'tanh'
-            ))
-
-        model.add(layers.Dense(30))
-
-        optimizer = optimizers.Adam(learning_rate = 0.001)
-        model.compile(optimizer = optimizer, loss = 'mse', metrics=['accuracy'])
-
-        return model
-      
-    def evaluate_model(self, df, trained_model, train_forecast = 240, forecast = 30):
-        predictions = pd.DataFrame(index = df.index, columns = [df.columns[0]])
-        for j in range(train_forecast, len(df) - train_forecast, forecast):
-            X = df[-j - train_forecast:-j]
-
-            # Prediction per train_forecast size
-            y_hat = model.predict(np.array(X).reshape(1, train_forecast, df.shape[0]))
-
-            # Update predictions
-            pred_df = pd.DataFrame(y_hat, index=pd.date_range(start=X.index[-1], periods=len(y_hat), freq="B"), columns=[X.columns[0]])
-            predictions.update(pred_df) 
-
-            
+   
